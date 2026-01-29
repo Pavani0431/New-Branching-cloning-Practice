dda9e78670f03553ed485ed927cd2b61189933e9
pavanibantu <pavanibantu1998@gmail.com>
2026-01-29 10:59:03 +0530

    updated java files

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,15 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.out.println("Hello New World!");
+  }
+}
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello New Java World!");
+  }
+}
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello Java World!");
   }
 }
